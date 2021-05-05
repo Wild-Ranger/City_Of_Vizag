6dbd1f3e8888028c654a0863e0a928562fe80e45
Wild-Ranger <n.ravivarma846@gmail.com>
2021-05-05 12:09:15 +0530

    vizag website

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className='footer-container'>
+            <section className="footer-subscription">
+                <p className="footer-subscription-heading">
+                    Have Fun Visiting Visakhapatnam
+                </p>
+                <p className="footer-subscription-text">
+                    Keep It Clean
+                </p>
+
+            </section>
+
+        </div>
+    )
+}
+
+export default Footer;
